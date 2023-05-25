2d3a0f7be30faaa1969eb4ef15325e7ba33064ed
Lorival-Jr <lorivaljunior0123@gmail.com>
2023-05-25 20:28:20 -0300

    Update gera_relacao.py
    corrigindo alguns bugs e organizando a saída e entrada de dados em pastas

--- gera_relacao.py
+++ gera_relacao.py
@@ -1,14 +1,16 @@
 # Relacao de escritorio
 import pandas as pd
 
+
 # Função para ler os dados
 def ler_dados():
+    import pandas as pd
     opcao = '_'
     while (opcao not in ('1', '2')):
         opcao = input("1 - Já tenho o banco completo \n2 - Quero que o programa una os csv's individuais\n\n")
     if opcao == '1':
         try:
-            banco = input('Qual o nome do seu banco (EX: Exportado_2_3_123.csv)\n\n')
+            banco = './exportados/' + input('Qual o nome do seu banco (EX: Exportado_2_3_123.csv)\n\n')
             dados = pd.read_csv(banco)
         except:
             try:
@@ -16,28 +18,30 @@ def ler_dados():
                 dados = pd.read_csv(banco)
             except:
                 from glob import glob
-                arquivos = glob('*.csv')
-                print(f'\n\nOs arquivos na sua pasta são {arquivos}, reveja o nome do arquivo digitado')
-                while (banco not in arquivos):
+                arquivos = glob('./exportados/' + '*.csv', )
+                arquivos_visual = [i.split('/')[-1] for i in arquivos]
+                print(f'\n\nOs arquivos na sua pasta são {arquivos_visual}, reveja o nome do arquivo digitado')
+                while (banco not in arquivos_visual):
                     banco = input('Qual o nome do seu banco (EX: Exportado_2_3_123.csv)\n\n')
-                dados = pd.read_csv(banco)
-        else:
-            input(
-                "Coloque todos csv's na pasta exportados, deixe apenas os arquivos fornecidos pelo SIPEIA, aperte <ENTER> para continuar")
-            from glob import glob
-            arquivos = glob('./exportados/*.csv')
-            for i in range(len(arquivos)):
-                path = arquivos[i]
-                if (i == 0):
-                    dados = pd.read_csv(path)
-                else:
-                    df = pd.read_csv(path)
-                    dados = pd.concat([dados, df], axis=0)
+                dados = pd.read_csv('./exportados/' + banco)
+    else:
+        input(
+            "Coloque todos csv's na pasta exportados, deixe apenas os arquivos fornecidos pelo SIPEIA, aperte <ENTER> para continuar\n")
+        from glob import glob
+        arquivos = glob('./exportados/*.csv')
+
+        for i in range(len(arquivos)):
+            if (i == 0):
+                dados = pd.read_csv(arquivos[i])
+            else:
+                df = pd.read_csv(arquivos[i])
+                dados = pd.concat([dados, df], axis=0)
 
     dados = dados[['CNPJ', 'Razão Social', 'Pesquisa', 'Modelo', 'Status da Empresa', 'Escritório do Contador', 'FAC']]
 
     dados.fillna(' ', inplace=True)
     dados.columns = ['CNPJ', 'Razão Social', 'Questionário', 'Modelo', 'Já entregue', 'Escritório', 'FAC']
+    dados.reset_index(inplace=True)
 
     # Correção das FAC's
     facs = dados[dados['FAC'].isin([8, 12, 13])].copy()
@@ -48,6 +52,7 @@ def ler_dados():
     dados['Já entregue'] = dados['Já entregue'].replace(
         ['NADA FEITO', 'ABORDAGEM EM ANDAMENTO', 'ABORDADA', 'ACORDADA', 'EM ATRASO', 'RENEGOCIADA', 'COBRADA',
          'NOTIFICADA', 'COLETADA', 'FAC'], ['Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Sim', 'FAC*'])
+    del dados['FAC']
     return (dados)
 
 
@@ -76,11 +81,12 @@ if (opcao == '1'):
 
     if ('FAC Concluída*' in dados['Já entregue']):
         dados = pd.concat((dados,
-                           pd.DataFrame(['* FAC Concluída: Empresa foi retirada da pesquisa neste ano'], columns=['CNPJ'])),
-            ignore_index=True, axis=0)
+                           pd.DataFrame(['* FAC Concluída: Empresa foi retirada da pesquisa neste ano'],
+                                        columns=['CNPJ'])),
+                          ignore_index=True, axis=0)
 
     dados.fillna(' ', inplace=True)
-    dados.to_excel('Lista Econômicas Anuais.xlsx', index=False)
+    dados.to_excel('./relacoes/' + 'Lista Econômicas Anuais.xlsx', index=False)
 
 elif (opcao == '2'):
     dados = ler_dados()
@@ -98,5 +104,5 @@ elif (opcao == '2'):
             dados_temp = pd.concat((dados, pd.DataFrame(['* FAC Concluída: Empresa foi retirada da pesquisa neste ano'],
                                                         columns=['CNPJ'])), ignore_index=True, axis=0)
 
-        dados_temp.fillna(' ', inplace=True)
-        dados_temp.to_excel(esc + ' - Econômicas Anuais.xlsx', index=False)
+        dados_temp = dados_temp.fillna(' ')
+        dados_temp.to_excel('./relacoes/' + esc + ' - Econômicas Anuais.xlsx', index=False)
